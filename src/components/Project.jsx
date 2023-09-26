0ddc5ce5aef667decf5e5a97d596441fa65e9e13
Mey-LinMus <mey-lin.mus@student.ehb.be>
2023-09-26 14:49:24 +0200

    Animation with ball and keys working

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -11,15 +11,25 @@ const Project = () => {
     const ball = ballRef.current;
 
     const animations = {
-      A: { x: -100, y: 0, scale: 1 },
+      A: { x: -100, y: 0, scale: 2 },
       Z: { x: 100, y: 0, scale: 1 },
       E: { x: 0, y: -100, scale: 1 },
       R: { x: 0, y: 100, scale: 1 },
-      T: { x: 0, y: 200, scale: 1 },
+      T: { x: 0, y: 200, scale: 5 },
       Y: { x: 0, y: 300, scale: 1 },
     };
 
     const defaultAnimation = { x: 0, y: 0, scale: 0.5 };
+
+    // Define the note frequencies for each key
+    const noteFrequencies = {
+      A: "C4",
+      Z: "D4",
+      E: "E4",
+      R: "F4",
+      T: "G4",
+      Y: "A4",
+    };
 
     // Function to play the animation based on the key
     const playAnimation = (key) => {
@@ -33,30 +43,35 @@ const Project = () => {
       });
     };
 
-    const playNote = (note, keyElement) => {
-      synth.triggerAttackRelease(note, "8n");
-      gsap.to(keyElement, {
-        duration: 0.2,
-        scaleY: 0.8,
-        yoyo: true,
-        repeat: 1,
-      });
+    const playNote = (key, keyElement) => {
+      const note = noteFrequencies[key];
+      if (note) {
+        synth.triggerAttackRelease(note, "4n");
+        gsap.to(keyElement, {
+          duration: 0.2,
+          scaleY: 0.8,
+          yoyo: true,
+          repeat: 1,
+        });
 
-      // Dispatch a custom event with the key as detail
-      const event = new CustomEvent("keyClick", {
-        detail: { key: keyElement.id },
-      });
-      document.dispatchEvent(event);
+        // Dispatch a custom event with the key as detail
+        const event = new CustomEvent("keyClick", {
+          detail: { key: keyElement.id },
+        });
+        document.dispatchEvent(event);
+      }
     };
 
     const keys = ["A", "Z", "E", "R", "T", "Y"];
 
-    keys.forEach((key) => {
-      const keyElement = document.getElementById(key);
-      keyElement.addEventListener("click", () => {
-        playNote("C4", keyElement);
+    // Listen for keydown events on the document
+    document.addEventListener("keydown", (event) => {
+      const key = event.key.toUpperCase(); // Convert the pressed key to uppercase
+      if (keys.includes(key)) {
+        const keyElement = document.getElementById(key);
+        playNote(key, keyElement);
         playAnimation(key);
-      });
+      }
     });
 
     return () => {
@@ -67,7 +82,6 @@ const Project = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Piano App</h1>
         <div className="piano">
           <div className="white-key" id="A"></div>
           <div className="white-key" id="Z"></div>
