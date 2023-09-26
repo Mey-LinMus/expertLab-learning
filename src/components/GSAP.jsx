dbb29d787bc1a07b03141e4898ea93e38929494b
Mey-LinMus <mey-lin.mus@student.ehb.be>
2023-09-26 09:06:43 +0200

    Reordening structure

--- src/components/GSAP.jsx
+++ src/components/GSAP.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { gsap } from "gsap";
-import "./App.css";
+import "../style/Gsap.css";
 
 const { useLayoutEffect, useRef } = React;
 
@@ -13,24 +13,24 @@ const Nested = () => {
   );
 };
 
-function App() {
+function GSAP() {
   const app = useRef();
   const circle = useRef();
   
   useLayoutEffect(() => {
     let ctx = gsap.context(() => {
       gsap.to(".box", { 
-        rotation: "+=360", 
-        duration: 0.5, 
+        rotation: 360,
         repeat: -1,
-        ease: 'easeIn'
+        repeatDelay: 1,
+        yoyo: true
       });
       
       gsap.to(circle.current, { 
-        rotation: "+=360", 
-        duration: 3, 
+        x: 100,
         repeat: -1,
-        ease: 'none'
+        repeatDelay: 1,
+        yoyo: true
       });
     }, app);
     
@@ -46,4 +46,4 @@ function App() {
   );
 }
 
-export default App;
+export default GSAP;
